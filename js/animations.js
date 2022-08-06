854195a13c8d45c9b485282e9e738f95480f4b13
algeps <vit7527@gmail.com>
2022-08-07 01:10:45 +0900

    Доп поле ввода при выборе бюджета панели

--- js/animations.js
+++ js/animations.js
@@ -18,4 +18,24 @@ function initIncreaseNumberAnimation() {
     increaseNumberAnimationStep(0, element, 5000);
 }
 
+document.querySelector("#budget").addEventListener('change', function handleSelectChange(event) {
+    const otherInput = document.querySelector(".form__other-input")
+
+    if(event.target.value === 'other') {
+        const formContainer = document.createElement("div")
+        formContainer.classList.add("form__group", "form__other-input")
+
+        const input = document.createElement("input")
+        input.setAttribute("placeholder", "Введите ваш вариант")
+        input.type = "text"
+
+        formContainer.appendChild(input)
+
+        document.querySelector("#form form").insertBefore(formContainer, document.querySelector(".form__submit"))
+    }
+    if(event.target.value !== 'other' && Boolean(otherInput)) {
+        document.querySelector("#form form").removeChild(otherInput)
+    }
+})
+
 initIncreaseNumberAnimation();
